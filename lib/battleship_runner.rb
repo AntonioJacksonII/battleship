8729d5746334936872d1ceb930751d3bdc97ff76
Antonio Jackson <antoniojackson@gmail.com>
2020-03-29 16:39:00 -0600

    Updating battleship runner to return player to main menu after each turn

--- lib/battleship_runner.rb
+++ lib/battleship_runner.rb
@@ -1,17 +1,33 @@
 require './lib/turn'
 require './lib/setup'
 
-p 'Welcome to BATTLESHIP'
-p 'Enter p to play. Enter q to quit.'
+def main_menu
+  @start = ""
+  p 'Welcome to BATTLESHIP'
+  p 'Enter p to play. Enter q to quit.'
+  @start = gets.chomp
 
-start = gets.chomp
-
-if start == 'p'
-  turn = Turn.new
-  turn.setup_game
-  p turn.determine_winner
-elsif start == 'q'
-  p 'You have quit the game'
-else
-  p "You did not enter 'p' or 'q'. Please try again."
+  if @start == 'p'
+    turn = Turn.new
+    turn.setup_game
+    p turn.determine_winner
+    main_menu
+  elsif @start == 'q'
+    p 'You have quit the game'
+  else
+    p "You did not enter 'p' or 'q'. Please try again."
+  end
 end
+main_menu
+
+# main_menu
+# if @start == 'p'
+#   turn = Turn.new
+#   turn.setup_game
+#   p turn.determine_winner
+#   main_menu
+# elsif @start == 'q'
+#   p 'You have quit the game'
+# else
+#   p "You did not enter 'p' or 'q'. Please try again."
+# end
