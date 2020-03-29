d97cb3bdd7811aa9336b5dd4a6184793eea994d3
Antonio Jackson <antoniojackson@gmail.com>
2020-03-29 14:48:36 -0600

    Updating render method to start with a new line

--- test/board_test.rb
+++ test/board_test.rb
@@ -81,11 +81,11 @@ class BoardTest < Minitest::Test
 
   def test_render
     board = Board.new
-    assert_equal "  1 2 3 4 \nA . . . . \nB . . . . \nC . . . . \nD . . . . \n", board.render
+    assert_equal "\n  1 2 3 4 \nA . . . . \nB . . . . \nC . . . . \nD . . . . \n", board.render
 
     cruiser = Ship.new("Cruiser", 3)
     board.place(cruiser, ["A1", "A2", "A3"])
-    assert_equal "  1 2 3 4 \nA . . . . \nB . . . . \nC . . . . \nD . . . . \n", board.render
-    assert_equal "  1 2 3 4 \nA S S S . \nB . . . . \nC . . . . \nD . . . . \n", board.render(true)
+    assert_equal "\n  1 2 3 4 \nA . . . . \nB . . . . \nC . . . . \nD . . . . \n", board.render
+    assert_equal "\n  1 2 3 4 \nA S S S . \nB . . . . \nC . . . . \nD . . . . \n", board.render(true)
   end
 end
