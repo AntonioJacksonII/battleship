adeb1cd6c1ee6cf9376b21e53f615ad251a83124
Antonio Jackson <antoniojackson@gmail.com>
2020-03-29 14:45:54 -0600

    Updating battleship runner with player and computer shots

--- lib/battleship_runner.rb
+++ lib/battleship_runner.rb
@@ -1,4 +1,5 @@
-require './lib/play'
+require './lib/turn'
+require './lib/setup'
 
 p 'Welcome to BATTLESHIP'
 p 'Enter p to play. Enter q to quit.'
@@ -8,8 +9,12 @@ start = gets.chomp
 if start == 'p'
   turn = Turn.new
   turn.setup_game
-  print turn.display_computer_board
-  print turn.display_player_board
+  print turn.display_both_boards
+  turn.get_player_shot
+  turn.validate_player_shot
+  p turn.take_player_shot
+  turn.get_computer_shot
+  turn.take_computer_shot
 elsif start == 'q'
   p 'You have quit the game'
 else
